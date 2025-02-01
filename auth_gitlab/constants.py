c1a31c6a28174c446bbb5f0917751521a2f37559
CookieGMVN <tuyentrandinh00@gmail.com>
2025-02-02 01:15:10 +0700

    fix: Constants error.

--- auth_gitlab/constants.py
+++ auth_gitlab/constants.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 
 CLIENT_ID = settings.GITLAB_APP_ID
-CLIENT_SECRET = settings.GITLAB_API_SECRET
-REQUIRE_VERIFIED_EMAIL = settings.GITLAB_REQUIRE_VERIFIED_EMAIL
+CLIENT_SECRET = settings.GITLAB_APP_SECRET
+REQUIRE_VERIFIED_EMAIL = getattr(settings, "GITLAB_REQUIRE_VERIFIED_EMAIL", 1)
 
 ERR_NO_GROUP_ACCESS = "You do not have access to the required GitLab group."
 ERR_NO_EMAIL = "We were unable to find an email address associated with your GitLab account."
@@ -10,7 +10,7 @@ ERR_NO_EMAIL = "We were unable to find an email address associated with your Git
 SCOPE = "read_user read_api"
 
 BASE_DOMAIN = settings.GITLAB_BASE_DOMAIN
-API_DOMAIN = settings.GITLAB_API_DOMAIN
 
+API_DOMAIN = f"https://{BASE_DOMAIN}/api/v4"
 ACCESS_TOKEN_URL = f"https://{BASE_DOMAIN}/oauth/token"
 AUTHORIZE_URL = f"https://{BASE_DOMAIN}/oauth/authorize"
